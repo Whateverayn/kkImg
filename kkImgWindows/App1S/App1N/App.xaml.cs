80152d44227af2d8ee157b60a8f7b21cc44be795
Whateverayn <94300252+Whateverayn@users.noreply.github.com>
2026-02-28 12:50:51 +0900

    feat: Add initial WinUI application project 'App1N' including core UI components and project configuration.

---
+++ kkImgWindows/App1S/App1N/App.xaml.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Controls.Primitives;
+using Microsoft.UI.Xaml.Data;
+using Microsoft.UI.Xaml.Input;
+using Microsoft.UI.Xaml.Media;
+using Microsoft.UI.Xaml.Navigation;
+using Microsoft.UI.Xaml.Shapes;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.ApplicationModel;
+using Windows.ApplicationModel.Activation;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace App1N
+{
+    /// <summary>
+    /// Provides application-specific behavior to supplement the default Application class.
+    /// </summary>
+    public partial class App : Application
+    {
+        // アプリ全体から今のアクティブを参照できるように static で定義
+        public static Window? ActiveWindow { get; private set; }
+
+        private Window? _window;
+
+        /// <summary>
+        /// Initializes the singleton application object.  This is the first line of authored code
+        /// executed, and as such is the logical equivalent of main() or WinMain().
+        /// </summary>
+        public App()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Invoked when the application is launched.
+        /// </summary>
+        /// <param name="args">Details about the launch request and process.</param>
+        protected override void OnLaunched(Microsoft.UI.Xaml.LaunchActivatedEventArgs args)
+        {
+            _window = new MainWindow();
+
+            // ウィンドウが手前に来た時に ActiveWindow を更新する設定
+            _window.Activated += (sender, e) =>
+            {
+                if (e.WindowActivationState != WindowActivationState.Deactivated)
+                {
+                    ActiveWindow = (Window)sender;
+                }
+            };
+
+            _window.Activate();
+
+            // 起動直後のアクティブウィンドウとして登録
+            ActiveWindow = _window;
+        }
+    }
+}
---
+++ kkImgWindows/App1S/App1N/kkImgTitleBar.xaml.cs
@@ -0,0 +1,30 @@
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Controls.Primitives;
+using Microsoft.UI.Xaml.Data;
+using Microsoft.UI.Xaml.Input;
+using Microsoft.UI.Xaml.Media;
+using Microsoft.UI.Xaml.Navigation;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace App1N
+{
+    public sealed partial class KkImgTitleBar : UserControl
+    {
+        public MainViewModel ViewModel { get; } = (MainViewModel)App.Current.Resources["MainViewModel"];
+
+        public KkImgTitleBar()
+        {
+            InitializeComponent();
+        }
+    }
+}
